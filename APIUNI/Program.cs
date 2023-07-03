c03f2229602b59f1af48a832612e39f8407f27b3
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-07-03 00:36:45 -0500

    Habilitar CORS

--- APIUNI/Program.cs
+++ APIUNI/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:42010",
+                          policy.WithOrigins("http://localhost:4200",
                                               "http://www.contoso.com")
                                               .AllowAnyHeader()
                                               .AllowAnyMethod();
